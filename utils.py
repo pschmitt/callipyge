ca36012292084bf7473a9e55513d7c5c6d51aec9
Philipp Schmitt <philipp@schmitt.co>
2014-06-08 03:51:48 +0200

    Add shell_exec() function

--- utils.py
+++ utils.py
@@ -1,6 +1,10 @@
 '''
-
+Various helper functions
 '''
+
+import subprocess
+import sys
+
 
 def decompose(string):
     '''
@@ -9,6 +13,26 @@ def decompose(string):
     dec = string.split('@', 1)
     return dec[0], dec[1]
 
+
+def shell_exec(cmd, verbose=False):
+    '''
+    Execute a shell command an return its output
+    Returns a list
+    '''
+    if verbose:
+        print('Execute shell command %s' % cmd)
+    try:
+        output = subprocess.check_output(cmd, shell=True)
+    except Exception as err:
+        if verbose:
+            print('Caught an exception {}'.format(err), file=sys.stderr)
+        return None
+    if verbose:
+        print('Output: %s' % output)
+    return output
+
 if __name__ == '__main__':
     assert decompose('root@localhost') == ('root', 'localhost')
+    assert shell_exec('echo -en test') == b'test'
     print('All test passed successfully')
+
