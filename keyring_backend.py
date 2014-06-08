b6f2f968d00fb4abe1d4535b2071f977ef485c51
Philipp Schmitt <philipp@schmitt.co>
2014-06-08 03:51:28 +0200

    First NON FUNCTIONNAL draft

--- keyring_backend.py
+++ keyring_backend.py
@@ -2,8 +2,15 @@
 
 '''
 
-import keyring
+from models.users import Users
+from models.access_rights import AccessRights
+from models.credentials import Credentials
+from models.inventory import Inventory
 from keyring.backend import KeyringBackend
+from utils import shell_exec
+import hashlib
+import keyring
+import os
 
 
 class CallipygeBackend(KeyringBackend):
@@ -12,30 +19,44 @@ class CallipygeBackend(KeyringBackend):
     '''
     priority = 1
 
-    def __init__(self):
+    def __init__(self, username):
+        self.user = username
+
+    def getencrPwd(self, something):
+        pass
+
+    def getAccesspwd(self, service, username):
+        pass
+
+    def check_access_right(self, host, account_name):
+        '''
+        Check whether a given user has the right to access some credentials
+        Return the ID of the corresponding AccessRights and -1 if not allowed
+        '''
         pass
 
     def get_password(self, service, username):
         '''
         Read the LIST (!) of password
         '''
-	list=username.split('@',1)
-	if len(list) !=2
-		raise NameError("host format must be user@host")
-	bool=getAccesspwd(list[0],list[1])
-	if bool:
-        	cipher=getencrPwd(list[0])
-        	decrPwd=echo "$cipher" | openssl rsautl -decrypt -inkey $USERNAME_private.pem
-        	connectTo(this.user,host,decrPwd)
+        access_id = self.check_access_right(service, username)
+        if access_id != -1:
+            cipher = self.getencrPwd(0)
+            decrPwd = shell_exec('openssl rsautl -decrypt -inkey ' +
+                                 username + '_private.pem <<<' + cipher)
         else:
-		raise NameError("you don't have access to requested host using this user")
-	pass
+            raise NameError("You don't have access to requested host using this user")
+        return decrPwd
 
     def set_password(self, service, username, password):
         '''
         Set a (new) password
         '''
-        pass
+        str = 'openssl rsautl -encrypt -inkey ' + self.user + \
+              '_public.pem -pubin <<< ' + password
+        res = shell_exec(str, verbose=True)
+        Inventory.create(hostname=service, account_name=username,
+                         account_password=res)
 
     def delete_password(self, service, username):
         '''
@@ -43,9 +64,26 @@ class CallipygeBackend(KeyringBackend):
         '''
         pass
 
+    def define_access_rights(self, service, user, account):
+        inv = Inventory.get(hostname=service, account_name=account)
+        ar = AccessRights.create(created_by=self.user.id,
+                                 user=user.id, host=inv.id)
+        str = 'echo '+inv.account_password+' | openssl rsautl -decrypt -inkey PATH/'+self.user+'_private.pem'
+        plain = shell_exec(str, verbose=True)
+        str = 'openssl rsautl -encrypt -inkey ' + user + '_public.pem -pubin <<< ' + plain
+        cipher = shell_exec(str, verbose=True)
+        Credentials.create(access=ar.id, password=cipher)
+
+    def register_user(username, password, passphrase):
+        shell_exec('openssl genrsa -aes256 -out '+username+'_private.pem -passout pass:'+passphrase+' 4096')
+        shell_exec('openssl rsa -in '+username+'_private.pem -outform PEM -pubout -out '+username+'_public.pem')
+        # TODO find some CSPRNG in python to feed the salt
+        salt = os.urandom(512)
+        pwd = hashlib.sha512(password.encode('utf-8')+salt).hexdigest()
+        Users.create(username=username, salt=salt, password=pwd)
 
 if __name__ == '__main__':
-    c = CallipygeBackend()
+    c = CallipygeBackend("root")
     keyring.set_keyring(c)
     keyring.set_password('test@localhost', 'root', 'test')
     assert keyring.get_password('test@localhost', 'root') == 'test'
